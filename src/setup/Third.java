8aa8ebcec8b5fb7ba1be2334b3b8594e1cc1feb8
Rashid <salehovr@gmail.com>
2021-10-03 12:43:43 -0400

    added 3rd file

---
+++ src/setup/Third.java
@@ -0,0 +1,13 @@
+package setup;
+
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+
+public class Third {
+    public static void main(String[] args) {
+
+        System.out.println("New third file");
+
+
+
+    }
+}
